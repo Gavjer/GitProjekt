3a7315e6e4d964818a30f4d99077394aad0fa096
Kiryl Antonik <gavjer@yandex.ru>
2019-06-25 08:52:31 +0200

    Form name

--- GitProjekt/Form1.Designer.cs
+++ GitProjekt/Form1.Designer.cs
@@ -261,7 +261,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Calkulator";
             this.ResumeLayout(false);
             this.PerformLayout();
 
